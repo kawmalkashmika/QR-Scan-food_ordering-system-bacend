587d1641461ce8aaf576a62b2ab339bbbbe7022d
Kawmal Kashmika <kkashmika@gmail.com>
2024-03-02 01:43:51 +0530

    Add table reservation pin API

--- routes/table.js
+++ routes/table.js
@@ -18,4 +18,24 @@ router.get('/get-all-table-details', (req, res) => {
     dbConnection.closeConnection(connection);
 });
 
+router.get('/get-reservation-pin',(req, res)=>{
+    let {tableId,userId}=req.body;
+
+    //check current tables has already active reservation
+
+
+
+    const connection = dbConnection.createConnection();
+    connection.query('SELECT ID_LOCATION_TABLE,ID_LOCATION_SECTION,TABLE_NAME,IS_ACTIVE FROM core_pos_location_table WHERE IS_ACTIVE=1 ;', (error, results, fields) => {
+        if (error) {
+            logger.error('Error retrieving data from database',error);
+            res.status(500).send('Error retrieving data from database');
+        }
+        res.status(200);
+        res.json(results);
+    });
+
+    dbConnection.closeConnection(connection);
+});
+
 module.exports=router;
