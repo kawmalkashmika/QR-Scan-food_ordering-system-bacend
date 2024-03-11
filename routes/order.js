6b87b2121403204f9a9de38eb591452051980e92
Kawmal Kashmika <kkashmika@gmail.com>
2024-03-11 14:49:35 +0530

    Add edit order API

--- routes/order.js
+++ routes/order.js
@@ -3,18 +3,19 @@ const logger = require('../util/logger');
 const express = require('express');
 const router = express.Router();
 const commonResponse = require('../commonResponse/commonResponse');
-const {response} = require("express");
+const {response, request} = require("express");
 const {error} = require("winston");
 const orderStatus = {
     PLACED: 1,
     PROGRESS: 2,
-    CANCEL:3
+    SERVED:3,
+    CANCELED:0,
 }
 
 const getItemPriceQuery="SELECT SELLING_PRICE FROM core_inv_item WHERE Id_Item=?";
 const placeOrderQuery="INSERT INTO core_mobile_reservation_order (`RESERVATION_ID`, `USER_ID`, `ITEM_ID`, `ORDER_STATUS`,`QUANTITY`) VALUES (?,?,?,?,?)";
-
-router.post('/calculate_bill', (req, res) => {
+let checkOrderStatusQuery="SELECT ORDER_STATUS FROM core_mobile_reservation_order WHERE RESERVATION_ORDER_ID=?";
+router.post('/calculate-current-bill', (req, res) => {
     let reservationId=req.body.reservationId;
     let userId=req.body.userId;
     let itemList=req.body.itemList;
@@ -64,7 +65,6 @@ router.post('/calculate_bill', (req, res) => {
     }, req.requestId);
 });
 
-
 /**
  * @swagger
  * /order/place-order:
@@ -111,7 +111,8 @@ router.post('/place-order',(req,res)=>{
             placeSingleOrder(connection,reservationId,userId,itemId,quantity).then(results=>{
                 console.log(results.insertId);
                 commonResponse.sendSuccessResponse(res,{"orderId":results.insertId,
-                                                              "orderStatus":orderStatus.PLACED},req.requestId);
+                                                            "orderStatus":orderStatus.PLACED,
+                                                            },req.requestId);
             }).catch(error=>{
                 commonResponse.sendErrorResponse(res,"Unable ti place order",req.requestId,500)
             })
@@ -119,18 +120,97 @@ router.post('/place-order',(req,res)=>{
         }
     },req.requestId)
 
+})
 
+/**
+ * @swagger
+ * /order/edit-order:
+ *   post:
+ *     summary: Edit an existing order
+ *     description: Edit the status or quantity of an existing order. At least one of orderStatus or quantity should be provided.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               orderId:
+ *                 type: string
+ *                 description: The ID of the order to edit.
+ *               orderStatus:
+ *                 type: string
+ *                 description: The new status of the order.
+ *               quantity:
+ *                 type: integer
+ *                 description: The new quantity of the order.
+ *     responses:
+ *       200:
+ *         description: Success message indicating the order was successfully edited.
+ *       400:
+ *         description: Bad request if both orderStatus and quantity are missing in the request.
+ */
+router.post('/edit-order',(req,res)=>{
+    let orderId=req.body.orderId;
+    let orderStatus=req.body.orderStatus;
+    let quantity=req.body.quantity;
 
+    let updateOrderQuery="UPDATE core_mobile_reservation_order SET  ";
+
+    if(orderStatus!=""){
+        let updateStatus=" ORDER_STATUS="+orderStatus
+        updateOrderQuery=updateOrderQuery+updateStatus;
+    }
+    if(quantity!=""){
+        let updateQuantity=" QUANTITY="+quantity
+        updateOrderQuery=updateOrderQuery+updateQuantity
+    }
+
+    console.log(updateOrderQuery);
+
+    dbConnection.getConnectionFromPool((err,connection)=>{
+        if(err){
+            logger.error("Unable to connect to database");
+            commonResponse.sendErrorResponse(res,"Unable to connect database",req.requestId,500);
+        }else{
+            connection.query(checkOrderStatusQuery,[orderId],(error,results,fields)=>{
+                if(error){
+                    logger.error("Unable to retrieve data from database",error);
+                    commonResponse.sendErrorResponse(res,"Unable to retrieve data from database",req.requestId,500);
+                }else{
+                    if(results[0].ORDER_STATUS===1){
+                        connection.query(updateOrderQuery+" WHERE RESERVATION_ORDER_ID=?",[orderId],(error,results,fields)=>{
+                            if(error){
+                                logger.error("Unable to edit order status");
+                                commonResponse.sendErrorResponse(res,"Unable to edit table status",req.requestId,500)
+                            }else{
+                                commonResponse.sendSuccessResponse(res,{
+                                    "orderId":orderId,
+                                    "OrderStatus":orderStatus,
+                                    "quantity":quantity
+                                },req.requestId);
+                            }
+
+                        })
+                    }else{
+                        logger.error("Unable to change order in current status");
+                        commonResponse.sendErrorResponse(res,"Unable to change order in current status",req.requestId,400)
+                    }
+                }
+            })
+        }
+    },req.requestId)
 
 
 })
+
+
 
 function placeSingleOrder(connection,reservationId,userId,itemId,quantity){
     return new Promise((resolve,reject)=>{
             connection.query(placeOrderQuery,[reservationId,userId,itemId,orderStatus.PLACED,quantity],(error,results,fields)=>{
                 if(error){
-                    logger.info(4)
-                    console.log("Unable to insert order details");
+                   logger.error("Unable to insert order",error)
                     reject(error);
                 }else{
                    resolve(results);
@@ -139,8 +219,6 @@ function placeSingleOrder(connection,reservationId,userId,itemId,quantity){
 
     });
 }
-
-
 function calculateBill(itemPrice,quantity){
     return itemPrice*quantity;
 }
