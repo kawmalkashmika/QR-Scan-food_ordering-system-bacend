0d9d300db415542ff2f6941c1aaa0f95c1d46a70
Kawmal Kashmika <kkashmika@gmail.com>
2024-03-11 12:35:43 +0530

    Add place order API

--- routes/order.js
+++ routes/order.js
@@ -4,9 +4,15 @@ const express = require('express');
 const router = express.Router();
 const commonResponse = require('../commonResponse/commonResponse');
 const {response} = require("express");
+const {error} = require("winston");
+const orderStatus = {
+    PLACED: 1,
+    PROGRESS: 2,
+    CANCEL:3
+}
 
 const getItemPriceQuery="SELECT SELLING_PRICE FROM core_inv_item WHERE Id_Item=?";
-const saveOrderQuery="INSERT INTO "
+const placeOrderQuery="INSERT INTO core_mobile_reservation_order (`RESERVATION_ID`, `USER_ID`, `ITEM_ID`, `ORDER_STATUS`,`QUANTITY`) VALUES (?,?,?,?,?)";
 
 router.post('/calculate_bill', (req, res) => {
     let reservationId=req.body.reservationId;
@@ -58,6 +64,81 @@ router.post('/calculate_bill', (req, res) => {
     }, req.requestId);
 });
 
+
+/**
+ * @swagger
+ * /order/place-order:
+ *   post:
+ *     summary: Place an order
+ *     description: Endpoint to place an order for a single item.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               reservationId:
+ *                 type: string
+ *                 description: The ID of the reservation.
+ *               userId:
+ *                 type: string
+ *                 description: The ID of the user placing the order.
+ *               itemId:
+ *                 type: string
+ *                 description: The ID of the item being ordered.
+ *               quantity:
+ *                 type: integer
+ *                 description: The quantity of the item being ordered.
+ *     responses:
+ *       200:
+ *         description: Successfully placed the order
+ *       500:
+ *         description: Unable to place the order
+ */
+router.post('/place-order',(req,res)=>{
+    let reservationId=req.body.reservationId;
+    let userId=req.body.userId;
+    let itemId=req.body.itemId;
+    let quantity=req.body.quantity;
+
+
+    dbConnection.getConnectionFromPool((error,connection)=>{
+        if(error){
+            logger.error("Unable to connect to database");
+            commonResponse.sendErrorResponse(res,"Unable to connect database",req.requestId,500)
+        }else{
+            placeSingleOrder(connection,reservationId,userId,itemId,quantity).then(results=>{
+                console.log(results.insertId);
+                commonResponse.sendSuccessResponse(res,{"orderId":results.insertId,
+                                                              "orderStatus":orderStatus.PLACED},req.requestId);
+            }).catch(error=>{
+                commonResponse.sendErrorResponse(res,"Unable ti place order",req.requestId,500)
+            })
+
+        }
+    },req.requestId)
+
+
+
+
+
+})
+
+function placeSingleOrder(connection,reservationId,userId,itemId,quantity){
+    return new Promise((resolve,reject)=>{
+            connection.query(placeOrderQuery,[reservationId,userId,itemId,orderStatus.PLACED,quantity],(error,results,fields)=>{
+                if(error){
+                    logger.info(4)
+                    console.log("Unable to insert order details");
+                    reject(error);
+                }else{
+                   resolve(results);
+                }
+            });
+
+    });
+}
 
 
 function calculateBill(itemPrice,quantity){
