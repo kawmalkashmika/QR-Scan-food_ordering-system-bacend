82b42c666516ff47c60e29ac7cc634009e7c32da
Kawmal Kashmika <40078584+kawmalkashmika@users.noreply.github.com>
2024-03-15 10:52:24 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -10,14 +10,15 @@ function getDatabaseCredentials(){
         }
     } else if(environment==ENV.QA){
         return {
-            host: '203.94.75.114',
+             host: 'localhost',
+            port: 3306,
             user: 'root',
+            password: 'Kawmal@123',
             database: 'h2_pos_harpos_bayleaf_rest',
-            port: "3306",
             waitForConnections: true,
             connectionLimit: 100,
             queueLimit: 0,
-            password: 'mrCat@_456',
+        
         }
     }else if(environment==ENV.PROD) {
         return {
